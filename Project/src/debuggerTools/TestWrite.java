291a867fd837aa8488dcdbccd66c603278dd7ca4
Brendan Loyot <b.loyot@umiami.edu>
2012-03-29 00:16:43 -0400

    added the schedule tab and a button which launches the scheduler. Added the scheduler executable to the bin, a launcher icon, and a few small changes

---
+++ Project/src/debuggerTools/TestWrite.java
@@ -0,0 +1,36 @@
+package debuggerTools;
+
+import java.util.List;
+import progAdmin.Employee;
+import progAdmin.PersonnelManager;
+
+public class TestWrite {
+//-----------------------------------------------------------------------------	 
+	public static void main(String[] args) {
+		//RosterParser read = new RosterParser();
+		List<Employee> readConfig = PersonnelManager.getRoster("src/utilities/xml/roster.xml");
+		
+		for (Employee employee : readConfig) {
+			System.out.println(employee);
+		}
+		
+	/*	
+
+
+		RosterWriter configFile = new RosterWriter();
+		configFile.setFile("src/utilities/xml/test_out.xml");
+		
+		try {
+			configFile.setList(readConfig);
+			configFile.saveConfig();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		*/
+		
+		
+	}
+//-----------------------------------------------------------------------------	 
+}
---
+++ Project/src/scheduleTab/ScheduleTab.java
@@ -0,0 +1,59 @@
+package scheduleTab;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import utilities.SwingHelper;
+
+public class ScheduleTab extends JPanel implements ActionListener {
+
+	static final int BUTTON_PANEL_WIDTH = 200;
+	static final int BUTTON_PANEL_HEIGHT = 200;
+	
+	JFrame parent;
+	String currentDir = System.getProperty("user.dir");
+	
+	public ScheduleTab(JFrame parent) {
+		this.setLayout(new BorderLayout());
+		this.parent = parent;
+		
+	    //resize and change later
+		JButton launchButton = SwingHelper.createImageButton("Launch Scheduler", "icons/launcher.png");
+		launchButton.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e){
+				// on click, launch the scheduler
+				launchScheduler();
+			}
+		});
+		
+		JPanel buttonPanel = new JPanel();
+        buttonPanel.add(launchButton);
+        this.add(buttonPanel, BorderLayout.SOUTH);
+	
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void launchScheduler() {
+		//launch schedule program
+		try {
+			Process process = Runtime.getRuntime().exec(currentDir + "/bin/UMPatrolSchedule.exe");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Could not launch Scheduler");
+			System.out.println("Attempted to launch in directory currentDir/bin");
+		}
+	}
+}
